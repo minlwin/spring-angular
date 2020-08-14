1e42b34b81018272bd5437088d4884a6ede8698c
minlwin <lwin.zawmin@gmail.com>
2020-08-14 10:54:52 +0630

    Share Spring Project

---
+++ bus-project/bus-api/src/main/java/com/jdc/bus/BusApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApiApplication.class, args);
+	}
+
+}
---
+++ bus-project/bus-api/src/test/java/com/jdc/bus/BusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
